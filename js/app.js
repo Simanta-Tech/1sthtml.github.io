010f880b499adaae759993ac9737beb0098fc444
Simanta-Tech <simanta.barua1>
2020-08-20 20:29:33 +0600

    second commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    //card option
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'blank',
+        img: 'images/blank.png'
+    },
+    {
+        name:'hotdog',
+        img:'images/hotdog.png'
+    },
+    {
+        name:'cheeseburger.png',
+        img: 'images/cheesburger.png'
+    },
+    {
+        name: 'ice-cream.png',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake.png',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img:  'images/piza.png'
+    },
+    {
+        name: 'white',
+        img: 'images/white.png'
+    }
+
+]
+const grid = document.querySelector.apply('.grid')
+
+})
